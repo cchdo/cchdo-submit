0c8b227e176322939207a2a4e4bf070d5d3c1455
Andrew Barna <abarna@gmail.com>
2021-09-10 00:51:24 +0000

    Start on CRA based submit form

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,44 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import {Form, Button} from 'react-bootstrap';
+
+const handleFormSubmit = (event:React.SyntheticEvent) => {
+  event.preventDefault();
+  const target = event.nativeEvent.target as HTMLFormElement
+  const data = new FormData(target);
+  console.log(Object.fromEntries(data.entries()))
+}
+
+const File = ({id}:{id:number}) => {
+      return (
+      <Form.Group className="mb-3" controlId="submitter_email">
+        <Form.Label>Your Email</Form.Label>
+        <Form.Control name={"file_whatever" + id} type="file" />
+      </Form.Group>
+      )
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Form onSubmit={handleFormSubmit}>
+      <Form.Group className="mb-3" controlId="submitter_name">
+        <Form.Label>Your Name</Form.Label>
+        <Form.Control name="submitter_name" type="text" placeholder="Your Name" />
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="submitter_email">
+        <Form.Label>Your Email</Form.Label>
+        <Form.Control name="submitter_email" type="email" placeholder="example@example.edu" />
+      </Form.Group>
+
+      <File id={0} />
+      <File id={2} />
+
+      <Button variant="primary" type="submit">
+        Submit
+      </Button>
+    </Form>
   );
 }
 
